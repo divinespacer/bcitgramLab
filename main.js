30e30c833d98361ae042067cb573ee3842a3f326
David Laptop <davidnguyenantran@gmail.com>
2023-10-15 18:00:21 -0700

    starter code

--- main.js
+++ main.js
@@ -13,7 +13,3 @@ const IOhandler = require("./IOhandler");
 const zipFilePath = path.join(__dirname, "myfile.zip");
 const pathUnzipped = path.join(__dirname, "unzipped");
 const pathProcessed = path.join(__dirname, "grayscaled");
-const unzipper = require("unzipper");
-const fs = require("fs");
-
-fs.createReadStream(zipFilePath).pipe(unzipper.Extract({ path: "./unzipped" }));
